196aacdb8b74690db23cc788ba532d05fa66a6b9
huseynhasan <huseynhasan80@gmail.com>
2026-03-18 11:08:40 +0400

    Clean Category on Store Page

--- store/views.py
+++ store/views.py
@@ -10,32 +10,19 @@ from django.db.models import Q
 # Create your views here.
 
 
-
 def store(request, category_slug=None):
-    categories = None
-    products   = None
+    products = Product.objects.filter(is_available=True)
 
-    if category_slug != None:
-        categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=categories, is_available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
 
-#         paginator = Paginator(products, 6)
-#         page = request.GET.get('page')
-#         page_products = paginator.get_page(page)
-
-        product_count = products.count()
-    else:
-        products = Product.objects.all().filter(is_available=True).order_by('id')
-
-#         paginator = Paginator(products, 6)
-#         page = request.GET.get('page')
-#         page_products = paginator.get_page(page)
-
-        product_count = products.count()
+    products = products.order_by('id')
+    product_count = products.count()
 
     context = {
-        'products':products,
-        'product_count':product_count,
+        'products': products,
+        'product_count': product_count,
     }
 
     return render(request, 'store/store.html', context)
