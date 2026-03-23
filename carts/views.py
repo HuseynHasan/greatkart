5fe0160ed00c96d5aeb3a0d53727b9079155400c
huseynhasan <huseynhasan80@gmail.com>
2026-03-23 14:10:50 +0400

    Optimized Cart Item Quantity, Increment, Decrement, Tax Calculation

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from carts.models import Cart, CartItem
 from store.models import Product
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -32,7 +34,56 @@ def add_cart(request, product_id):
     return redirect('cart')
 
 
+def remove_cart(request, product_id):
+    cart_id = _cart_id(request)
+
+    cart_item = CartItem.objects.filter(
+        cart__cart_id=cart_id,
+        product_id=product_id
+    ).first()
+
+    if not cart_item:
+        return redirect('cart')
+
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save(update_fields=['quantity'])
+    else:
+        cart_item.delete()
+
+    return redirect('cart')
+
+
+def remove_cart_item(request, product_id):
+    CartItem.objects.filter(
+        cart__cart_id=_cart_id(request),
+        product_id=product_id
+    ).delete()
+
+    return redirect('cart')
+
+
 
 def cart(request):
+    total = 0
+    quantity = 0
 
-    return render(request, 'store/cart.html')
+    cart_items = CartItem.objects.filter(
+        cart__cart_id=_cart_id(request),
+        is_active=True
+    ).select_related('product')
+
+    for item in cart_items:
+        total += item.product.price * item.quantity
+        quantity += item.quantity
+
+    tax = (2 * total) / 100
+    grand_total = total + tax
+
+    return render(request, 'store/cart.html', {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total,
+    })
