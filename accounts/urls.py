7eedc795a650cda59197f25b80a949d9eab83261
huseynhasan <huseynhasan80@gmail.com>
2026-04-07 15:00:41 +0400

    dashboard

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('logout/', views.logout, name='logout'),
     path('login/', views.login, name='login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
 
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 ]
